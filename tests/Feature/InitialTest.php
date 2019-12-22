2cc3f662718d563d7f23707059edb478d2157b14
Mohammed <mohammed@heber-net.net>
2019-12-22 13:33:45 +0100

    inital setup

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Laragen\Tests\Feature;
+
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+
+    /**
+         * @test
+    */
+    public function first_test()
+    {
+        $this->assertTrue(true);
+
+    }
+
+}
