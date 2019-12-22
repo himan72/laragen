8a11be6358dd90344eb81a69e89d2db3f759d05e
Mohammed <mohammed@heber-net.net>
2019-12-22 13:42:16 +0100

    create laragen service provider

---
+++ src/LaragenServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Laragen;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LaragenServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+
+    }
+
+}
