dbcb1a9f19c2f359f4dd0a0fc1e2e2d6dbfac169
Mohammed <mohammed@heber-net.net>
2019-12-29 16:22:45 +0100

    refactoring builder

--- src/Builders/ModelFileBuilder.php
+++ src/Builders/ModelFileBuilder.php
@@ -4,10 +4,10 @@ namespace Laragen\Builders;
 
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Arr;
-use Laragen\Contracts\Builder;
+use Laragen\Contracts\FileBuilder;
 use Laragen\Entities\Model;
 
-class ModelBuilder implements Builder
+class ModelFileBuilder implements FileBuilder
 {
     /**
      * @var Model
@@ -22,7 +22,7 @@ class ModelBuilder implements Builder
     /**
      * @var string
      */
-    protected $body = '';
+    protected $body;
 
     /**
      * ModelBuilder constructor.
@@ -30,45 +30,52 @@ class ModelBuilder implements Builder
      * @param Model $model
      * @param $files
      */
-    public function __construct(Model $model, $files)
+    public function __construct(Model $model)
     {
 
         $this->model = $model;
 
-        $this->files = $files;
+        $this->files = new Filesystem();
     }
 
     /**
      * build  the model class file for output
      *
      */
-    public function build()
+    public function execute()
     {
         $modelfile = str_replace('//...', $this->buildBody(), $this->buildClass());
 
         $this->files->put(dirname(__DIR__)."/{$this->model->name()}.php", $modelfile);
     }
 
-    private function buildClass()
+    protected function buildClass()
     {
         $model_class_body = $this->files->get(dirname(__DIR__).'/stubs/model/class.stub');
 
-        $model_class_body = str_replace('ClassPlaceHolder', $this->model->name(), $model_class_body);
-        $model_class_body = str_replace('NamespacePlaceHolder', $this->model->namespace(), $model_class_body);
+        $model_class_body = str_replace(['ClassPlaceHolder', 'NamespacePlaceHolder'],
+            [$this->model->name(), $this->model->namespace()], $model_class_body);
+
+        if ($this->model->usesSoftDeletes()) {
+            $model_class_body = $this->addSoftDeletes($model_class_body);
+        }
 
         return $model_class_body;
     }
 
-    private function buildBody()
+    protected function buildBody()
     {
-        $this->buildFillables()->buildDates()->buildCasts();
+        $this->addFillables()
+            ->addDates()
+            ->addCasts()
+            ->addRouteKeyName();
 
         return $this->body;
-        // todo relations, soft delete table primarykey route key
+        // todo relations, table primarykey
 
     }
 
-    private function buildFillables()
+    private function addFillables()
     {
         if (! empty($fillables = $this->model->fillables())) {
             $stub = $this->files->get(dirname(__DIR__).'/stubs/model/fillable.stub');
@@ -82,10 +89,8 @@ class ModelBuilder implements Builder
         return $this;
     }
 
-    private function buildDates()
+    private function addDates()
     {
-
-
         if (isset($this->model->casts()['datetime'])) {
 
             $dates = $this->model->casts()['datetime'];
@@ -102,22 +107,59 @@ class ModelBuilder implements Builder
         return $this;
     }
 
-    private function buildCasts()
+    private function addCasts()
     {
 
         if (! empty($this->model->casts())) {
 
             $casts = Arr::except($this->model->casts(), ['datetime']);
-            $cast_arr = '';
+
+            $cast_array = '';
+
             foreach ($casts as $cast_type => $proerties) {
                 $attrs = preg_split('/\s+/', trim($proerties));
                 foreach ($attrs as $attr) {
-                    $cast_arr .= "'{$attr}' => '{$cast_type}',\n";
+                    $cast_array .= "\t\t'{$attr}' => '{$cast_type}',".PHP_EOL;
                 }
             }
             $stub = $this->files->get(dirname(__DIR__).'/stubs/model/casts.stub');
 
-            $this->body .= PHP_EOL.str_replace('//...', $cast_arr, $stub);
+            $this->body .= PHP_EOL.str_replace('//...', $cast_array, $stub);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add softDeletes Trait
+     *
+     * @param string $model_class_stub
+     * @return string|string[]
+     */
+    private function addSoftDeletes(string $model_class_stub)
+    {
+        $model_class_stub = str_replace(
+            'use Illuminate\\Database\\Eloquent\\Model;',
+            'use Illuminate\\Database\\Eloquent\\Model;'.PHP_EOL.'use Illuminate\\Database\\Eloquent\\SoftDeletes;',
+            $model_class_stub);
+
+        $model_class_stub = preg_replace('/^\{$/m', '{'.PHP_EOL.'    use SoftDeletes;', $model_class_stub);
+
+        return $model_class_stub;
+    }
+
+    private function addRouteKeyName()
+    {
+        if ($this->model->routeKeyName() != 'id') {
+
+            $stub = $this->files->get(dirname(__DIR__).'/stubs/model/method.stub');
+
+            $stub = str_replace(
+                ['PlaceHolderName', 'null'],
+                ['getRouteKeyName', "'{$this->model->routeKeyName()}'"],
+                $stub);
+
+            $this->body .= PHP_EOL.$stub;
         }
 
         return $this;
